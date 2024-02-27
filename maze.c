4aff223a4d54989936e2ccb280066990b039d911
seifzogheibi <146748894+seifzogheibi@users.noreply.github.com>
2024-02-27 17:47:29 +0000

    prog proj

---
+++ maze.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+void read_maze(char *filename){
+    printf("Invalid size");
+}
+
+int main(int argc, char **argv){
+
+}
+
+int is_player_position_S(int x, int y){
+    //start describing 
+
+    // you want to do in this code
+
+    //just using comments
+
+    //and you basically tell amy
+
+    //what she needs to do in the code
+
+    //in this function
+}
+
+//do the same for diferent functions with different function names and then in the main function write comments that explain
+//that you will call the different functions you defined but from the main() funciton
+
+
+//do the same for typedef struct
+
+//you can writr your comments the way you like( paragrphs or comments or cutted comments idk idc just do it)
+
+//when you need just to define any variable you can do it as it is not actual code
+//for e.g : 
+
+#define rows 14;
+#define cols 18;
+//bas you put this in the beginning of your code , so only define and comments all over the codes.
+
+//conclusion : this is fors writing functions and commenting every single one of them and explaining what she needs to implements if you give her this skeleton code you understand you piece of shit
+
--- test.sh
+++ test.sh
@@ -1,113 +1,223 @@
-#Testing for correct file input
+###FILE###
+
+#Testing if a file is selected
 ./maze > tmp
-if grep -q "Usage: ./maze <filename>" tmp
+if grep -q "File not specified." tmp;
 then
     echo "PASS"
 else
     echo "FAIL"
 fi
 
-#SHAPE/SIZE(3)
-
-#Testing maze shape
-./maze ireg_height_5x5.txt > tmp
-if grep -q "Maze format not in rectangluar shape." tmp
+#Testing if the file exists
+./maze okokok > tmp
+if grep -q "File does not exist." tmp;
 then
-  echo "PASS"
+    echo "PASS"
 else
     echo "FAIL"
 fi
 
-#Testing maze size
-./maze ireg_width_5x5.txt > tmp
-if grep -q "Invalid row size." tmp
+
+###ENDPOINTS###
+
+#Testing if the maze has a Start point
+./maze reg_7x7.txt > tmp 
+if grep -q "The maze has no starting point!" tmp;
 then
-  echo "PASS"
+    echo "PASS"
 else
     echo "FAIL"
 fi
 
-#Testing maze size
-./maze ireg_height_5x5 > tmp
-if grep -q "Invalid column size." tmp
+#Testing if the maze has an End point
+./maze reg_7x7.txt > tmp 
+if grep -q "The maze has no exit point!" tmp;
 then
-  echo "PASS"
+    echo "PASS"
 else
     echo "FAIL"
 fi
 
-#MOVEMENTS(4)
 
-#Testing (w) movement up
-./maze reg_15x8.txt < movement_up > tmp
-if grep -q "You moved up." tmp
+###EXIT###
+
+#Testing if reaching E completes
+./maze reg_10x6.txt > tmp
+if grep -q "Congrats You completed thr" tmp;
 then
-  echo "PASS"
+    echo "PASS"
 else
     echo "FAIL"
 fi
 
-#Testing (s) movement down
-./maze reg_15x8.txt < movement_down > tmp
-if grep -q "You moved down." tmp
+
+###SIZE/SHAPE###
+
+#Checking maze size (rows) 
+./maze ireg_9x2.txt > tmp 
+if grep -q "Invalid rows." tmp;
 then
-  echo "PASS"
+    echo "PASS"
 else
     echo "FAIL"
 fi
 
-#Testing (a) movement left
-./maze reg_15x8.txt < movement_left > tmp
-if grep -q "You moved left." tmp
+#Checking maze size (columns)
+./maze ireg_2x5.txt > tmp 
+if grep -q "Invalid columns." tmp;
 then
-  echo "PASS"
+    echo "PASS"
 else
     echo "FAIL"
 fi
 
-#Testing (d) movement right
-./maze reg_15x8.txt < movement_right > tmp
-if grep -q "You moved right." tmp
+
+#Checking maze shape (rows)
+./maze ireg_width_5x5.txt > tmp 
+if grep -q "Bad dimensions. Maze must be rectangular." tmp;
 then
-  echo "PASS"
+    echo "PASS"
 else
     echo "FAIL"
 fi
 
-#WALLS(4)
-
-#Testing (w) movement up but hitting a wall
-./maze reg_10x6.txt < movement_up > tmp
-if grep -q "Movement upwards is blocked!" tmp
+#Checking maze shape (columns)
+./maze ireg_height_5x5.txt > tmp 
+if grep -q "Bad dimensions. Maze must be rectangular." tmp;
 then
-  echo "PASS"
+    echo "PASS"
 else
     echo "FAIL"
 fi
 
-#Testing (s) movement down but hitting a wall
-./maze reg_5x5.txt < movement_down > tmp
-if grep -q "Movement downwards is blocked!" tmp
+
+###INPUTS###
+
+#Testing for valid (W) input
+./maze reg_15x8.txt < movement_up.txt > tmp
+if grep -q "You moved up." tmp;
 then
-  echo "PASS"
+    echo "PASS"
 else
     echo "FAIL"
 fi
 
-#Testing (a) movement left but hitting a wall
-./maze reg_10x6.txt < movement_left > tmp
-if grep -q "Movement leftwards is blocked!" tmp
+#Testing for valid (A) input
+./maze reg_15x8.txt < movement_left.txt > tmp
+if grep -q "You moved left." tmp;
 then
-  echo "PASS"
+    echo "PASS"
 else
     echo "FAIL"
 fi
 
-#Testing (d) movement right but hitting a wall
-./maze reg_5x5.txt < movement_right > tmp
-if grep -q "Movement rightwards is blocked!" tmp
+#Testing for valid (S) input
+./maze reg_5x5.txt < movement_down.txt > tmp
+if grep -q "You moved down." tmp;
 then
-  echo "PASS"
+    echo "PASS"
+else
+    echo "FAIL"
+fi
+
+#Testing for valid (D) input
+./maze reg_5x5.txt < movement_right.txt > tmp
+if grep -q "You moved right." tmp;
+then
+    echo "PASS"
+else
+    echo "FAIL"
+fi
+
+#Testing for hitting a wall after a (W) input
+./maze reg_5x5.txt < movement_up.txt > tmp
+if grep -q "You hit the wall above!" tmp;
+then
+    echo "PASS"
+else
+    echo "FAIL"
+fi
+
+#Testing for hitting a wall after a (A) input
+./maze reg_5x5.txt < movement_left.txt > tmp
+if grep -q "You hit the wall to your left!" tmp;
+then
+    echo "PASS"
+else
+    echo "FAIL"
+fi
+
+#Testing for hitting a wall after a (S) input
+./maze reg_15x8.txt < movement_down.txt > tmp
+if grep -q "You hit the wall below!" tmp;
+then
+    echo "PASS"
+else
+    echo "FAIL"
+fi
+
+#Testing for hitting a wall after a (D) input
+./maze reg_15x8.txt < movement_right.txt > tmp
+if grep -q "You hit the wall to your right!" tmp;
+then
+    echo "PASS"
+else
+    echo "FAIL"
+fi 
+
+#Testing for out of bounds after a (W) input
+./maze reg_6x6.txt < movement_up.txt > tmp
+if grep -q "You cannot move up." tmp;
+then
+    echo "PASS"
+else
+    echo "FAIL"
+fi
+
+#Testing for out of bounds after a (A) input
+./maze reg_6x6.txt < movement_left.txt > tmp
+if grep -q "You cannot move left." tmp;
+then
+    echo "PASS"
+else
+    echo "FAIL"
+fi
+
+#Testing for out of bounds after a (S) input
+./maze reg_6x7.txt < movement_down.txt > tmp
+if grep -q "You cannot move down." tmp;
+then
+    echo "PASS"
+else
+    echo "FAIL"
+fi
+
+#Testing for out of bounds after a (D) input
+./maze reg_6x7.txt < movement_right.txt > tmp
+if grep -q "You cannot move right." tmp;
+then
+    echo "PASS"
+else
+    echo "FAIL"
+fi
+
+###SOLUTIONS/FAILS###
+
+#Testing a solution sequence
+./maze reg_6x7.txt < sol_6x7.txt > tmp
+if grep -q "This sequence completes the maze." tmp;
+then
+    echo "PASS"
+else
+    echo "FAIL"
+fi
+
+#Testing a failure sequence
+./maze reg_5x5.txt < sol_6x7.txt > tmp
+if grep -q "This sequence does not solve the maze." tmp;
+then
+    echo "PASS"
 else
     echo "FAIL"
 fi
